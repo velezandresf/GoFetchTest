f7d10a405d149428e0e72a9d8df6c86f6b36ceb5
Andres Velez <andresvelez@Andres-Giga-IT-MacBook-Pro.local>
2021-12-01 12:09:59 -0500

    feat add dashboard page

--- src/pages/dashboard/index.js
+++ src/pages/dashboard/index.js
@@ -1,9 +1,24 @@
-import React from 'react'
+import { Button } from '@mui/material';
+import React from 'react';
+import { useUserContext } from '../../context/userContext';
+
+import './dashboard.css';
 
 export const Dashboard = () => {
+    const { user, logoutUser } = useUserContext();
     return (
-        <div>
-            Dashboard
-        </div>
+         <div className='wraper'>
+            <div className='userInformation'>
+                {user.photoURL && 
+                    <img src={user.photoURL} alt='githubphoto' width='50px'/>
+                }
+                <p>Name : {user.displayName}</p>
+                <p>Email : {user.email}</p>
+            </div>
+            <div className='logoutButton'>
+                <Button variant='contained' onClick={logoutUser}>Log Out</Button>
+            </div>
+    </div>
+    
     )
 }
